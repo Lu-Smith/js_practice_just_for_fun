7042a358c7e17afd5b4250a7be981d336fa94ef5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-23 17:25:25 +0100

    Updated clock.js

---
+++ pages/clock.js
@@ -0,0 +1,19 @@
+setInterval(setClock, 1000);
+const hourHand = document.getElementById('hour-hand');
+const minuteHand = document.getElementById('minute-hand');
+const secondHand = document.getElementById('second-hand');
+
+
+function setClock() {
+    const currentDate = new Date();
+    const secondRatio = currentDate.getSeconds()/60;
+    const minuteRatio = (secondRatio + currentDate.getMinutes())/60;
+    const hourRatio = (minuteRatio + currentDate.getHours())/12;
+    setRotation(secondHand, secondRatio);
+    setRotation(minuteHand, minuteRatio);
+    setRotation(hourHand, hourRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
