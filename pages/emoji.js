09a1b4aa38aab385d4bfafa11fc41155f255023b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-23 12:58:56 +0100

    Updated emoji.js

--- pages/emoji.js
+++ pages/emoji.js
@@ -1,2 +1,16 @@
-const colsedFlower = document.getElementById('closed-flower');
+const closedFlower = document.getElementById('closed-flower');
 const openFlower = document.getElementById('open-flower');
+
+closedFlower.addEventListener('click', () => {
+    if(openFlower.classList.contains('open')) {
+          openFlower.classList.add('active');
+          closedFlower.classList.remove('active');
+    }
+});
+
+openFlower.addEventListener('click', () => {
+    if(closedFlower.classList.contains('closed')) {
+          closedFlower.classList.add('active');
+          openFlower.classList.remove('active');
+    }
+})
