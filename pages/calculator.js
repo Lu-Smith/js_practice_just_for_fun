84e65c8099b3e6fbf343e0c6eaec748a13d8c00a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-23 18:46:55 +0100

    Updated calculator.js

---
+++ pages/calculator.js
@@ -0,0 +1,35 @@
+class Calculator {
+    constructor(previousOperandElement, currentOperandElement){
+       this.previousOperandElement = previousOperandElement;
+       this.currentOperandElement = currentOperandElement;
+    }
+    clear() {
+
+    }
+    delate() {
+
+    }
+    appendNumber() {
+
+    }
+    chooseOperation() {
+
+    }
+    compute() {
+        
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelector('[data-equals]');
+const delateButton = document.querySelector('[data-delate]');
+const allClearButoon = document.querySelector('[data-all-clear]');
+const previousOperandElement = document.querySelector('[data-previous-operand]');
+const currentOperandElement = document.querySelector('[data-current-operand]');
+
+
+
+
+
